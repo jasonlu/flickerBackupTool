2cb42cee71f078a96cc44539f8d76d0d27ef41c4
Jason Lu <jasonl.biz@gmail.com>
2013-11-03 02:41:05 -0500

    init

---
+++ flickerBackupTool/FlickerController.h
@@ -0,0 +1,15 @@
+//
+//  FlickerController.h
+//  flickerBackupTool
+//
+//  Created by Jason Lu on 11/3/13.
+//  Copyright (c) 2013 Lu Jason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlickerController : NSObject
+extern NSString *const apiKey;
+extern NSString *const apiSecret;
+
+@end
---
+++ flickerBackupTool/FlickerController.m
@@ -0,0 +1,16 @@
+//
+//  FlickerController.m
+//  flickerBackupTool
+//
+//  Created by Jason Lu on 11/3/13.
+//  Copyright (c) 2013 Lu Jason. All rights reserved.
+//
+
+#import "FlickerController.h"
+
+@implementation FlickerController
+
+NSString *const apiKey = @"42b8daa9c251314e3737efd371dc7a4d";
+NSString *const apiSecret = @"0e702fcbf0e5224b";
+
+@end
