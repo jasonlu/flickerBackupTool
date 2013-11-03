4fc6b878114cbc91de03d1bdf181c4cae211980e
Jason Lu <jasonl.biz@gmail.com>
2013-11-03 01:24:50 -0500

    Initial Commit

---
+++ flickerBackupTool/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  flickerBackupTool
+//
+//  Created by Jason Lu on 11/3/13.
+//  Copyright (c) 2013 Lu Jason. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
